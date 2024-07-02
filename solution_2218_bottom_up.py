0262e8d686aaaf29f9e4bdaf0b713cc55d3cea72
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-07-01 20:28:57 -0600

    feat: 2218 bottom up

---
+++ solution_2218_bottom_up.py
@@ -0,0 +1,16 @@
+def maxValueOfCoins(piles, k):
+    n = len(piles)
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    for i in range(n - 1, -1, -1):
+        for remain in range(1, k + 1):
+            dp[i][remain] = dp[i + 1][remain]
+            curr = 0
+            for j in range(min(remain, len(piles[i]))):
+                curr += piles[i][j]
+                dp[i][remain] = max(dp[i][remain],
+                                    curr + dp[i + 1][remain - j - 1])
+
+    return dp[0][k]
+
+
+print(maxValueOfCoins([[1, 100, 3], [7, 8, 9]], 2))
