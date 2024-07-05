933ac812e7d2f7135963da4029e3f9c95c09e25c
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-07-04 18:40:26 -0600

    feat: solution 62

---
+++ solution_62.py
@@ -0,0 +1,21 @@
+from functools import cache
+
+
+def uniquePaths(m, n):
+    @cache
+    def dp(row, col):
+        if row + col == 0:
+            return 1
+
+        ways = 0
+        if row > 0:
+            ways += dp(row - 1, col)
+        if col > 0:
+            ways += dp(row, col - 1)
+
+        return ways
+
+    return dp(m - 1, n - 1)
+
+
+print(uniquePaths(3, 7))
