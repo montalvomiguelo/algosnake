f197e8012f1c9094705700be145d3e6c186b3810
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-07-12 21:56:04 -0600

    feat: day 01

--- aoc23/day_01.py
+++ aoc23/day_01.py
@@ -8,3 +8,15 @@ with open('day_01.txt') as f:
         ans += int(matches[0] + matches[-1])
 
     print(ans)
+
+with open('day_01.txt') as f:
+    lines = [l.rstrip('\n') for l in f]
+    dic = {'one': '1', 'two': '2', 'three': '3', 'four': '4',
+           'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+    ans = 0
+    for l in lines:
+        matches = re.findall(
+            r'\d|one|two|three|four|five|six|seven|eight|nine|ten', l)
+        ans += int(''.join([dic[d] if d in dic else d for d in [matches[0], matches[-1]]]))
+
+    print(ans)
