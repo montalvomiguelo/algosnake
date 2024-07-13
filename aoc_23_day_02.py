36eae3ae607e1571bf979d533aeaba7c8b2b895c
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-07-13 13:03:55 -0600

    refactor: day 02

--- aoc_23_day_02.py
+++ aoc_23_day_02.py
@@ -1,21 +1,17 @@
+import re
+
 with open('day_02.txt') as f:
     lines = [l.strip('\n') for l in f]
     dic = {'red': 12, 'green': 13, 'blue': 14}
     ans = 0
     for l in lines:
-        parts = l.split(': ')
-        id = parts[0]
-        sets = parts[1].split('; ')
-        isValid = True
-        for s in sets:
-            cubes = s.split(', ')
-            for c in cubes:
-                count = int(c.split()[0])
-                color = c.split()[1]
-                if count > dic[color]:
-                    isValid = False
-                    break
-        if isValid:
-            ans += int(id[5:])
+        id = int(re.search(r'(\d+):', l).group(1))
+        ans += id
+        matches = re.findall(r'(\d+)\s+(\w+)', l)
+        for match in matches:
+            count, color = match
+            if int(count) > dic[color]:
+                ans -= id
+                break
 
     print(ans)
