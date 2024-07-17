2ba3d69b870c905c8172e82982a876dcc0c9d15a
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-07-16 21:22:47 -0600

    feat: solution 47

---
+++ solution_47.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+
+def permuteUnique(nums):
+    def backtrack(curr, counts):
+        if len(curr) == len(nums):
+            ans.append(curr[:])
+            return
+
+        for num, freq in counts.items():
+            if freq > 0:
+                curr.append(num)
+                counts[num] -= 1
+                backtrack(curr, counts)
+                curr.pop()
+                counts[num] += 1
+
+    counts = defaultdict(int)
+    for num in nums:
+        counts[num] += 1
+
+    ans = []
+    backtrack([], counts)
+    return ans
+
+
+print(permuteUnique([1, 1, 2]))
