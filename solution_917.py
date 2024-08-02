610dbdbfeca70784e190f5ccb60e3eb79d9d6af6
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-08-01 20:53:57 -0600

    refactor: solution 917

--- solution_917.py
+++ solution_917.py
@@ -1,21 +1,14 @@
 def reverseOnlyLetters(s):
-    def isLetter(l):
-        code = ord(l)
-        if code < 65 or (code > 90 and code < 97):
-            return False
-
-        return True
-
     chars = list(s)
     left = 0
     right = len(chars) - 1
 
     while left < right:
-        if not isLetter(chars[left]):
+        if not chars[left].isalpha():
             left += 1
             continue
 
-        if not isLetter(chars[right]):
+        if not chars[right].isalpha():
             right -= 1
             continue
 
