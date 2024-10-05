7e0878cdbbc197334a3f79e8db5b98ab49adc78e
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-10-05 11:56:23 -0600

    feat: day 02
    part 2

--- aoc_23_day_02.py
+++ aoc_23_day_02.py
@@ -1,17 +1,36 @@
+from collections import defaultdict
 import re
 
 with open('day_02.txt') as f:
     lines = [l.strip('\n') for l in f]
-    dic = {'red': 12, 'green': 13, 'blue': 14}
-    ans = 0
-    for l in lines:
-        id = int(re.search(r'(\d+):', l).group(1))
-        ans += id
-        matches = re.findall(r'(\d+)\s+(\w+)', l)
-        for match in matches:
-            count, color = match
-            if int(count) > dic[color]:
-                ans -= id
-                break
 
-    print(ans)
+# Part 1
+dic = {'red': 12, 'green': 13, 'blue': 14}
+ans = 0
+for l in lines:
+    id = int(re.search(r'(\d+):', l).group(1))
+    ans += id
+    matches = re.findall(r'(\d+)\s+(\w+)', l)
+    for match in matches:
+        count, color = match
+        if int(count) > dic[color]:
+            ans -= id
+            break
+
+print(ans)
+
+# Part 2
+ans = 0
+for l in lines:
+    dic = defaultdict(int)
+    matches = re.findall(r'(\d+)\s+(\w+)', l)
+    res = 1
+    for count, color in matches:
+        dic[color] = max(dic[color], int(count))
+
+    for v in dic.values():
+        res *= v
+
+    ans += res
+
+print(ans)
