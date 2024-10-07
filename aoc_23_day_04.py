42a8992f423e3d912504930b548566bd765ddddf
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-10-06 19:45:11 -0600

    feat: day 04
    part 2

--- aoc_23_day_04.py
+++ aoc_23_day_04.py
@@ -1,3 +1,4 @@
+from functools import cache
 import re
 
 
@@ -7,15 +8,40 @@ with open('day_04.txt') as f:
 # Part 1
 ans = 0
 for l in lines:
-    matches = re.search(r'(\d+(?:\s+\d+)*)\s+\|\s*(\d+(?:\s+\d+)*)', l)
-    numbersSet = set(matches.group(1).split())
+    match = re.search(r'(\d+(?:\s+\d+)*)\s+\|\s+(\d+(?:\s+\d+)*)', l)
+    winningNumbers = set(match.group(1).split())
+    numbers = match.group(2).split()
     res, count = 1, 0
-    for number in matches.group(2).split():
-        if number in numbersSet:
+    for number in numbers:
+        if number in winningNumbers:
             res *= 1 if count == 0 else 2
             count += 1
 
     if count:
         ans += res
+
+print(ans)
+
+# Part 2
+
+
+@cache
+def dp(i):
+    l = lines[i]
+    match = re.search(r'(\d+(?:\s+\d+)*)\s+\|\s+(\d+(?:\s+\d+)*)', l)
+    winningNumbers = set(match.group(1).split())
+    numbers = match.group(2).split()
+    ans, j = 1, 1
+    for number in numbers:
+        if number in winningNumbers:
+            ans += dp(i + j)
+            j += 1
+    return ans
+
+
+ans = 0
+for i in range(len(lines)):
+    ret = dp(i)
+    ans += ret
 
 print(ans)
