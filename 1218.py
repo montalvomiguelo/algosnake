bff7d3151379bc05a96cd50f5bdd1a3eef3030b7
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-11-10 20:26:11 -0600

    refactor(1218): improved time complexity

--- 1218.py
+++ 1218.py
@@ -1,18 +1,10 @@
-from functools import cache
-
-
 def longestSubsequence(arr, difference):
-    @cache
-    def dp(i):
-        ans = 1
+    def dp(a):
+        memo[a] = memo.get(a - difference, 0) + 1
+        return memo[a]
 
-        for j in range(i):
-            if arr[i] - arr[j] == difference:
-                ans = max(ans, dp(j) + 1)
-
-        return ans
-
-    return max(dp(i) for i in range(len(arr)))
+    memo = {}
+    return max(dp(a) for a in arr)
 
 
 print(longestSubsequence([1, 5, 7, 8, 5, 3, 4, 2, 1], -2))
