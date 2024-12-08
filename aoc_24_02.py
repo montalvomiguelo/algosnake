3542d1f87221ff40b9e5a205b2a8c8a0aa60ab8a
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-12-07 22:37:11 -0600

    feat: aoc 24 day 02
    part 2

--- aoc_24_02.py
+++ aoc_24_02.py
@@ -32,3 +32,38 @@ for nums in data:
         ans += 1 if j == len(nums) - 1 else 0
 
 print(ans)
+
+# Part 2
+def isSafe(nums):
+    sorting = ''
+    for i in range(len(nums) - 1):
+        j = i + 1
+        if nums[i] == nums[j]:
+            return False
+
+        diff = abs(nums[i] - nums[j])
+        if diff < 1 or diff > 3:
+            return False
+
+        newSorting = 'asc' if nums[i] < nums[j] else 'dec'
+        if not sorting:
+            sorting = newSorting
+            continue
+
+        if newSorting != sorting:
+            return False
+
+    return True
+
+
+ans = 0
+for nums in data:
+    if isSafe(nums):
+        ans += 1
+        continue
+    for i in range(len(nums)):
+        if isSafe(nums[:i] + nums[i + 1:]):
+            ans += 1
+            break
+
+print(ans)
