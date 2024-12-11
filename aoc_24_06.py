96d6174410389e0b7a40b36b980dcf7643f811ef
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-12-11 00:21:07 -0600

    feat: aoc 24 day 06
    part 2

--- aoc_24_06.py
+++ aoc_24_06.py
@@ -1,5 +1,5 @@
 with open('aoc_24_06.txt') as f:
-    grid = [l.rstrip('\n') for l in f]
+    grid = [list(s) for s in [l.rstrip('\n') for l in f]]
 
 # Part 1
 def valid(row, col):
@@ -35,3 +35,52 @@ while stack:
             stack.append((nextRow, nextCol, i))
 
 print(len(seen))
+
+# Part 2
+root = (-1 , -1)
+for row in range(n):
+    for col in range(m):
+        if grid[row][col] == '^':
+            root = (row, col)
+            break
+
+ans = 0
+for row in range(n):
+    for col in range(m):
+        char = grid[row][col]
+        if char == '^' or char == '#':
+            continue
+
+        grid[row][col] = '#'
+        curr = root
+        seen = set()
+        i = 0
+        while True:
+            if (curr[0], curr[1], i) in seen:
+                ans += 1
+                break
+
+            seen.add((curr[0], curr[1], i))
+
+            dy, dx = directions[i]
+            nextRow, nextCol = curr[0] + dy, curr[1] + dx
+            if not valid(nextRow, nextCol):
+                break
+
+            if grid[nextRow][nextCol] == '#':
+                for j in range(1, 4):
+                    k = (j + i) % 4
+                    dy, dx = directions[k]
+                    nextRow, nextCol = curr[0] + dy, curr[1] + dx
+                    if grid[nextRow][nextCol] != '#':
+                        curr = (nextRow, nextCol)
+                        i = k
+                        break
+
+                continue
+
+            curr = (nextRow, nextCol)
+
+        grid[row][col] = char
+
+print(ans)
