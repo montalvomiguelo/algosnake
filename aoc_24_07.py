87ebeb6770d4bff25336a7741e604f827575c0d6
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-12-12 21:28:43 -0600

    feat: aoc 24 day 07
    part 2

--- aoc_24_07.py
+++ aoc_24_07.py
@@ -25,6 +25,30 @@ for nums in data:
         ans += nums[0]
 
 print(ans)
+
+# Part 2
+def backtrack(curr, i, target, nums):
+    if i == len(nums):
+        return curr == target
+
+    if curr > target:
+        return False
+
+    for j in range(i, len(nums)):
+        if backtrack(curr + nums[j], j + 1, target, nums):
+            return True
+
+        if backtrack(curr * nums[j], j + 1, target, nums):
+            return True
+
+        if backtrack(int(str(curr) + str(nums[j])), j + 1, target, nums):
+            return True
+
+    return False
+
+ans = 0
+for nums in data:
+    if backtrack(nums[1], 1, nums[0], nums[1:]):
         ans += nums[0]
 
 print(ans)
